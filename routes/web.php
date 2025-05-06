b1197eb1b8816b71edebaa4cc946e0a1ff895d80
Ilya Nurullin <nurullinin@mail.ru>
2025-05-06 23:21:58 +0500

    install laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/html', function () {
-    return view('first');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::view('/php', 'second', [
-    'show' => true,
-    'name' => request('name', 'Anon')
-]);
-
-Route::view('/blade', 'third', [
-    'show' => true,
-    'name' => request('name', 'Anon'),
-    'users' => ['John', 'Mike', 'Kate']
-]);
-
-Route::view('/page', 'page');
+require __DIR__.'/auth.php';
