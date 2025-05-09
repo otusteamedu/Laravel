72ab798d20cce889efb6ec1347672ebe8f97a966
pshenichkin_maxim <mix45@inbox.ru>
2025-05-09 12:12:06 +0300

    frontend: login page, register page, home page, start page, about page, forgot page; lang

---
+++ lang/en/main.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'services_not_found' => 'There are no services yet',
+    'not_name' => 'The name is not specified',
+    'more_details' => 'More details',
+];
---
+++ lang/ru/main.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'services_not_found' => 'Пока нет ни одной услуги',
+    'not_name' => 'Название не задано',
+    'more_details' => 'Подробнее',
+];
