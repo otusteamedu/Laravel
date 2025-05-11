105f603c46124d46fbb45fe3a21c83c2f1193f44
KirillNekrasov21012 <kirya.nekrasov.21012@gmail.com>
2025-05-12 01:10:12 +0300

    hw2: create 4 pages

---
+++ resources/views/auth/verify-email.blade.php
@@ -0,0 +1,31 @@
+<x-guest-layout>
+    <div class="mb-4 text-sm text-gray-600">
+        {{ __('account.verify_answer') }}
+    </div>
+
+    @if (session('status') == 'verification-link-sent')
+        <div class="mb-4 font-medium text-sm text-green-600">
+            {{ __('account.verify_send') }}
+        </div>
+    @endif
+
+    <div class="mt-4 flex items-center justify-between">
+        <form method="POST" action="{{ route('verification.send') }}">
+            @csrf
+
+            <div>
+                <x-primary-button>
+                    {{ __('account.verify_resend') }}
+                </x-primary-button>
+            </div>
+        </form>
+
+        <form method="POST" action="{{ route('logout') }}">
+            @csrf
+
+            <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                {{ __('account.log_out') }}
+            </button>
+        </form>
+    </div>
+</x-guest-layout>
