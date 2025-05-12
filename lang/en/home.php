c0135eb9d02dcc5b5bd7cb96f3b03cf4f25fb864
Vladimir Nesterov <vnesterov@wejet.ru>
2025-05-12 19:03:39 +0300

    ДЗ-2

---
+++ lang/en/home.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'title' => 'Welcome to Our App',
+    'description' => 'A modern Laravel application with Bootstrap integration. This template provides a responsive layout that works well on all devices.',
+    'sub_title' => 'Features'
+];
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\App;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/{locale}', function (string $locale) {
+    if (! in_array($locale, ['en', 'ru'])) {
+        abort(400);
+    }
+
+    App::setLocale($locale);
+    return view('home');
+});
+
+Route::get('/user', function () {
+    return view('user');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::get('/static', function () {
+    return view('static');
 });
