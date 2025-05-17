4bc4f60496f2f5d6bdd2db0f04f1e6543d7221c1
vhar <vhar@mail.ru>
2025-05-17 19:10:03 +0300

    fix:

---
+++ database/migrations/2025_05_17_142721_drop_joined_and_lent_coluns_from_todos_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('todos', function (Blueprint $table) {
+            $table->dropColumn('joined_at');
+            $table->dropColumn('left_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('todos', function (Blueprint $table) {
+            $table->timestamp('joined_at')->nullable();
+            $table->timestamp('left_at')->nullable();
+        });
+    }
+};
