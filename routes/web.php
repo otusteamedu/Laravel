01e9acb269c0a30998ca49cd2b5fc4b8c97463bb
o.lukianova <okondrashova@bk.ru>
2025-06-02 20:19:28 +0300

    revert

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProfileController;
-use App\Models\Todolist;
-use App\Queries\UserQueries;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/e', function (Request $request) {
-    dump(Todolist::create([
-        "title" => "task1",
-        "text" => "do my laravel homework",
-        "dedline" => "02.06.2025",
-        "author_id" => 1,
-    ]));
 
-return "";
-});
