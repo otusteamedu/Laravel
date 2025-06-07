2d1b0e568db2be0daad8bea2ca657642abe3a949
o.lukianova <okondrashova@bk.ru>
2025-06-07 18:54:39 +0300

    admin layout

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends ('layouts.main')
+
+@section ('body')
+    <h1>Здесь выводим список новостей</h1>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-// first commit
+Route::view('/admin', 'home')->name('home');
+Route::view('/create', 'create')->name('create');
