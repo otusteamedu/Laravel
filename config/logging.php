1f323db94d901b79028e47746fe95fd1823fbab4
o.lukianova <okondrashova@bk.ru>
2025-06-07 13:56:18 +0300

    logs to telegram channel

--- config/logging.php
+++ config/logging.php
@@ -3,7 +3,10 @@
 use Monolog\Handler\NullHandler;
 use Monolog\Handler\StreamHandler;
 use Monolog\Handler\SyslogUdpHandler;
+use Monolog\Handler\TelegramBotHandler;
 use Monolog\Processor\PsrLogMessageProcessor;
+
+// use Monolog\Processor\TelegramBotHandler;
 
 return [
 
@@ -63,6 +66,7 @@ return [
             'path' => storage_path('logs/laravel.log'),
             'level' => env('LOG_LEVEL', 'debug'),
             'replace_placeholders' => true,
+
         ],
 
         'daily' => [
@@ -125,6 +129,16 @@ return [
 
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
+        ],
+
+        'telegram' => [
+            'driver' => 'monolog',
+            'level' => env('LOG_LEVEL_TELEGRAM', 'error'),
+            'handler' => TelegramBotHandler::class,
+            'with' => [
+                'apiKey' => env('LOG_TELEGRAM_BOT_TOKEN'),
+                'channel' => env('LOG_TELEGRAM_CHAT_ID'),
+            ],
         ],
 
     ],
--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// first commit
+Route::get('/log', function (Request $request) {
+    // Ставим логи разных уровней и проверяем, чтоб в телеграм приходило только то, что не ниже уровня error
+
+    Log::channel('telegram')->debug('debug level error');
+    Log::channel('telegram')->info('info level error');
+    Log::channel('telegram')->error('error level error');
+    Log::channel('telegram')->critical('critical level error');
+    Log::channel('telegram')->emergency('emergency level error');
+
+    return ['ok' => true];
+});
