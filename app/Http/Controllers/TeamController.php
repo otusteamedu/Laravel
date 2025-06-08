07dc42e0d7e4cfa76f7685884cad2d21c50c72b8
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2025-06-09 00:48:57 +0300

    feat: добавлены политики для модели Team

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -10,6 +10,7 @@ use App\Services\Team\TeamsViewService;
 use App\Services\Team\TeamUpdateService;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Storage;
 use Throwable;
 
@@ -31,6 +32,7 @@ class TeamController extends Controller
      */
     public function create(): View
     {
+        Gate::authorize('team.create');
         return view('teams.create');
     }
 
@@ -77,6 +79,7 @@ class TeamController extends Controller
         TeamsViewService $teamsViewService,
     ): View
     {
+        Gate::authorize('team.update', $id);
         $data['team'] = $teamsViewService->fetchOne($id);
         return view('teams.edit', $data);
     }
@@ -122,6 +125,7 @@ class TeamController extends Controller
         TeamDestroyService $teamDestroyService,
     ): RedirectResponse
     {
+        Gate::authorize('team.delete', $id);
         try {
             $teamDestroyService->handle($id);
         } catch (Throwable $e) {
--- app/Models/User.php
+++ app/Models/User.php
@@ -65,4 +65,9 @@ class User extends Authenticatable
     {
         return $this->belongsTo(UserRole::class);
     }
+
+    public function isAdmin(): bool
+    {
+        return $this->role_id === 2;
+    }
 }
