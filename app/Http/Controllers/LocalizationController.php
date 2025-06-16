4ef1c517ae408ee10adaf57ffeed0cdb824ef478
vhar <vhar@mail.ru>
2025-06-16 11:34:22 +0300

    refactor(CodeReview): убрана дополнительная валидация запрошенной локали

--- app/Http/Controllers/LocalizationController.php
+++ app/Http/Controllers/LocalizationController.php
@@ -9,10 +9,6 @@ class LocalizationController extends Controller
 {
     public function __invoke(Request $request): RedirectResponse
     {
-        if (! in_array($request->locale, array_keys(config()->get('locale.available_locales')))) {
-            abort(400);
-        }
-
         $request->session()->put('locale', $request->locale);
 
         return redirect()->back();
