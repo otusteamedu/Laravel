a5f66704f9764f48b927c18efe2ddb4bba4a55c9
pshenichkin_maxim <mix45@inbox.ru>
2025-06-21 21:27:33 +0300

    hw11 # устранил проблему с прогревом кэша # Использование пакета, подключенного локально

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use PshenichkinMaxim\UserLogo\Facades\UserLogo;
 
 Route::get('/', function () {
     return view('welcome', [
@@ -13,3 +14,10 @@ Auth::routes();
 
 Route::get('/home', HomeController::class)->middleware(['auth'])->name('home');
 Route::view('/about', 'about');
+
+Route::get('/test-package', function (UserLogo $userLogo) {
+
+   // dd(UserLogo::generate('Иванов Иван'));
+    //dd(userlogo('Иванов Иван'));
+    return '';
+});
