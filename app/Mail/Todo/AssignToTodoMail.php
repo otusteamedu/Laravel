ce8cd9c4ad9918827beca58eaf6d597371c2ffc7
vhar <vhar@mail.ru>
2025-06-28 22:23:41 +0300

    feat: mail notification

---
+++ app/Mail/Todo/AssignToTodoMail.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Mail\Todo;
+
+use App\Models\TodoRoleEnum;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Address;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Mail\Mailables\Envelope;
+use App\Services\Repositories\DTOs\UserDTO;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Services\Repositories\DTOs\ProjectDTO;
+use App\Services\Repositories\Todo\TodoFetchDTO;
+
+
+class AssignToTodoMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     * @param UserDTO $user
+     * @param ProjectDTO $project
+     * @param TodoFetchDTO $todo
+     * @param TodoRoleEnum|null $role
+     */
+    public function __construct(
+        public UserDTO $user,
+        public ProjectDTO $project,
+        public TodoFetchDTO $todo,
+        public ?TodoRoleEnum $role = null,
+    ) {}
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        $from = Config::get('mail.from');
+
+        return new Envelope(
+            from: new Address($from['address'], $from['name']),
+            subject: "Вас назначили на роль {$this->role->value} в задаче {$this->todo->title}",
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content('notifications.mail.assign-todo-role');
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
