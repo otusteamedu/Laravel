2d4ad310099b7df949aced6dcce307a0fd276dbd
o.lukianova <okondrashova@bk.ru>
2025-06-29 18:25:04 +0300

    queue

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,24 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// first commit
+/*
+Сделаем очередь задач, получающих количество вакансий для заданного работодателя по его id с сайта headhunter.ru.
+В файл выводятся работодатели с количеством открытых вакансий больше 500.
+*/
+
+Route::get('/job', function (
+    \Illuminate\Contracts\Queue\Queue $queue,
+    \Illuminate\Contracts\Bus\Dispatcher $dispatcher,
+) {
+    $pageCount = 1000;
+
+    for ($i = 0; $i < $pageCount; $i++) {
+        $job = new \App\Jobs\GetVacancies($i);
+
+        dispatch($job);
+
+    }
+
+    return new \Illuminate\Http\Response('Задачи отправлены');
+
+});
