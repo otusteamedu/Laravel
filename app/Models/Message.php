e13afe44c20b190f7c2bf0656ee07f136b54a652
elisad5791 <elisad5791@mail.ru>
2025-07-06 19:02:51 +0500

    Add message model, migration, seeder

---
+++ app/Models/Message.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Carbon;
+
+/**
+ * Message model.
+ *
+ * @property int $id
+ * @property string $content
+ * @property int $user_id
+ * @property ?Carbon $created_at
+ * @property ?Carbon $updated_at
+ *
+ * @property-read User $user
+ */
+class Message extends Model
+{
+    /** @use HasFactory<\Database\Factories\MessageFactory> */
+    use HasFactory;
+
+    protected $guarded = [];
+    protected $table = 'messages';
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    public function getUserId(): int
+    {
+        return $this->user_id;
+    }
+
+    public function getCreatedAt(): Carbon
+    {
+        return $this->created_at;
+    }
+
+    public function getUpdatedAt(): Carbon
+    {
+        return $this->updated_at;
+    }
+
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
