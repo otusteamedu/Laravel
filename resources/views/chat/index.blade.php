b0f71ba0cfa60965981d4b3d6eb17540e7688eb5
elisad5791 <elisad5791@mail.ru>
2025-07-06 19:39:52 +0500

    Add html layout for chat

--- resources/views/chat/index.blade.php
+++ resources/views/chat/index.blade.php
@@ -6,14 +6,35 @@
             <h1 class="display-6 fw-bold text-center">Чат</h1>
         </div>
 
-        <div>
-            @foreach ($messages as $message)
-            <div>
-                <p>{{ $message->getContent() }}</p>
-                <p>{{ $message->getUser()->name }}</p>
-                <p>{{ $message->getCreatedAt()->format('d.m.Y H:i') }}</p>
+        <div class="row">
+            <div class="col-lg-8 mx-auto">
+                <div>
+                    @foreach ($messages as $message)
+                        <div>
+                            <p class="fw-bold">{{ $message->getUser()->name }},
+                                {{ $message->getCreatedAt()->format('d.m.Y H:i') }}</p>
+                            <p>{{ $message->getContent() }}</p>
+                        </div>
+                    @endforeach
+                </div>
+
+                @guest
+                <div>
+                    <a href="{{ route('login') }}" class="text-decoration-none">
+                        Авторизуйтесь, чтобы написать сообщение
+                    </a>
+                </div>
+                @endguest
+
+                @auth
+                <form>
+                    <div class="mb-3">
+                        <textarea class="form-control" rows="3" placeholder="Текст сообщения"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Отправить сообщение</button>
+                </form>
+                @endauth
             </div>
-            @endforeach
         </div>
     </div>
 @endsection
