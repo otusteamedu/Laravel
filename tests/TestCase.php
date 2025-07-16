ab35537ad1f638a476849ced18ff7ae8f0955e55
elisad5791 <elisad5791@mail.ru>
2025-07-16 20:15:55 +0500

    Add mock for meilisearch in tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,22 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Laravel\Scout\EngineManager;
+use Laravel\Scout\Engines\NullEngine;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+        
+        $this->app->extend(EngineManager::class, function ($manager, $app) {
+            return new class($app) extends EngineManager {
+                public function engine($name = null)
+                {
+                    return new NullEngine;
+                }
+            };
+        });
+    }
 }
