c17b416544fd4206f8f499be101d213a17f348c1
o.lukianova <okondrashova@bk.ru>
2025-07-20 13:39:38 +0300

    migration api_token

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,14 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 require __DIR__.'/auth.php';
 
 Route::apiResource('posts', \App\Http\Controllers\PostsController::class);
+
+Route::get('/user', function (Request $request) {
+    dump($request);
+
+    return $request->user();
+});
