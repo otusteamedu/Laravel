9aeaaecb18357661c5f713adcc025f67fc1db005
elisad5791 <elisad5791@mail.ru>
2025-07-27 17:40:04 +0500

    Add tests for api

--- routes/api.php
+++ routes/api.php
@@ -7,32 +7,25 @@ use App\Http\Controllers\Api\v2\SearchController;
 use App\Http\Controllers\Api\v3\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-function v1Routes()
-{
+Route::prefix('v1')->name('v1.')->middleware('auth:api')->group(function () {
     Route::apiResource('/categories', CategoryController::class);
     Route::apiResource('/products', ProductController::class);
-}
-
-function v2Routes()
-{
-    v1Routes();
-    Route::get('/search', SearchController::class);
-}
-
-Route::prefix('v1')->name('v1.')->middleware('auth:api')->group(function () {
-    v1Routes();
 });
 
 Route::prefix('v2')->name('v2.')->middleware('auth:api')->group(function () {
-    v2Routes();
+    Route::apiResource('/categories', CategoryController::class);
+    Route::apiResource('/products', ProductController::class);
+    Route::get('/search', SearchController::class);
 });
 
 Route::prefix('v3')->name('v3.')->middleware('auth:api')->group(function () {
-    v2Routes();
-    Route::get('/profile', [ProfileController::class, 'getProfile']);
-    Route::get('/orders', [ProfileController::class, 'getOrders']);
-    Route::post('/profile', [ProfileController::class, 'updateProfile']);
-    Route::post('/profile/password', [ProfileController::class, 'updatePassword']);
+    Route::apiResource('/categories', CategoryController::class);
+    Route::apiResource('/products', ProductController::class);
+    Route::get('/search', SearchController::class);
+    Route::get('/profile', [ProfileController::class, 'getProfile'])->name('profile.get');
+    Route::get('/profile/orders', [ProfileController::class, 'getOrders'])->name('profile.orders');
+    Route::patch('/profile', [ProfileController::class, 'updateProfile'])->name('profile.update');
+    Route::patch('/profile/password', [ProfileController::class, 'updatePassword'])->name('profile.password');
 });
 
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api');
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api')->name('api.logout');
