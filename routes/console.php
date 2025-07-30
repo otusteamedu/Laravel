2b9a7d019ad655ed5abe60c6230606a48d67506c
pshenichkin_maxim <mix45@inbox.ru>
2025-07-30 12:16:57 +0300

    hw19
    # планирование задач

--- routes/console.php
+++ routes/console.php
@@ -2,17 +2,7 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-use App\Console\Commands\WarmCacheCommand;
-use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
-
-/*Schedule::command(WarmCacheCommand::class, [
-    'news'
-])->everyFifteenMinutes();
-
-Schedule::command(WarmCacheCommand::class, [
-    'categories'
-])->everyThirtyMinutes();*/
