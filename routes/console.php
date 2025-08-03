32aadf1e16f0c95f65af2ad592c9bcefd398c771
vhar <vhar@mail.ru>
2025-08-03 11:12:19 +0300

    refactor(HomeWork): code review refactoring

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,7 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('cache:warmup')->daily();
+Schedule::command('cache:warmup')
+    ->daily()
+    ->runInBackground()
+    ->onOneServer();
