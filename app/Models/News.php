3a459f323a45bbd174e837b7d5f53bcc6bbc60fe
mrkamischinmailru <mr.kamischin@mail.ru>
2025-08-09 16:53:22 +0300

    feat(models): создание моделей+роутинг+связи (доп сиды и миграции)

--- app/Models/News.php
+++ app/Models/News.php
@@ -1,12 +1,42 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
-class News extends Model
+class News extends BaseModel
 {
+    use SoftDeletes;
+    public $timestamps = true;
+
+    protected $fillable = [
+        "name",
+        "text",
+        "user_id",
+        "link",
+        "preview",
+        'create_at'
+    ];
+
+    public $casts = ['user_id' => 'integer'];
+
     /** @use HasFactory<\Database\Factories\NewsFactory> */
     use HasFactory;
+
+    function preview()
+    {
+        return $this->hasOne(NewsPreview::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function likes()
+    {
+        return $this->morphMany(Like::class, 'liked');
+    }
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Test;
+use App\Models\News;
+use App\Queries\UserQueries;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
 Route::get('/', function () {
     return view('main',
         [
@@ -40,3 +46,51 @@ Route::get('/news/', function () {
 });
 
 Route::get('/test/', [Test::class, 'test']);
+
+Route::group(['prefix' => '/newsdb'], function(){
+    Route::get('/create', function (Request $request) {
+        dd(News::create([
+            "name" => e($request['name']),
+            "text" => e($request['text']),
+            "user_id" => (int)$request['user'],
+            "link" => Str::slug($request['name']),
+            "preview"=> e($request['text']),
+            'create_at'=>Carbon::now()->format('Y-m-d')
+        ]));
+        return;
+    });
+
+    Route::get("/update/{id}", function ($id) {
+        $post = News::find($id);
+        $post->name = "updated title";
+        dd($post->save());
+        return;
+    });
+
+    Route::get("/delete/{id}", function ($id) {
+        $news = News::withTrashed()->find($id);
+        dd($news->delete());
+        return;
+    });
+
+    Route::get('/one/{id}', function ($id) {
+        $news = News::find($id);
+        dd($news);
+        dd($news->preview->post);
+        return;
+    });
+
+    Route::get('/one-all', function () {
+        $news = News::all();
+        $news->load('preview');
+        dd($news);
+        foreach ($news as $post) {
+            $post->preview;
+        }
+        return;
+    });
+
+    Route::get("/poly/{num}", function ($num) {
+        dd(News::find($num));
+    });
+});
