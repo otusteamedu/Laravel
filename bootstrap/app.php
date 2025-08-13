c87424a0ad61d786454e3e7bb030cf3b166e01c8
o.lukianova <okondrashova@bk.ru>
2025-08-13 08:29:03 +0300

    cron task

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\DB;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -18,9 +19,23 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withSchedule(function (Schedule $schedule) {
-        // Пример задачи: ###
+        // Задача: добавлем каждую минуту по одной записи в таблицу puples
         $schedule->call(function () {
-            file_put_contents('log12345.txt', '111 222 ');
-        })->everyMinute(); // Выполнять задачу ежедневно
+            DB::table('puples')->insert(
+                [
+                    [
+                        'name' => fake('ru_RU')->firstName,
+                        'surname' => fake('ru_RU')->lastName,
+                        'date_of_birth' => fake()->dateTimeBetween('2016-01-01', '2017-06-30')->format('d-m-Y'),
+                        'gender' => fake()->randomElement(['male', 'female']),
+                        'email' => fake()->email,
+                        'created_at' => now(),
+                        'updated_at' => now(),
+                    ],
+
+                ]
+            );
+
+        })->everyMinute(); // Выполнять задачу ежеминутно
     })
     ->create();
