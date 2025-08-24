263246dd6079b62a47c74fbde33608826d45281c
o.lukianova <okondrashova@bk.ru>
2025-08-24 19:09:44 +0300

    little fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->validateCsrfTokens(except: [
-            '/webhook',
+            '/api/webhook',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
