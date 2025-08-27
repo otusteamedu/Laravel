d0baa020ea4b9656009d58c7aa6d8be408c60a66
o.lukianova <okondrashova@bk.ru>
2025-08-27 12:39:36 +0300

    add browser title

--- resources/views/layouts/root.blade.php
+++ resources/views/layouts/root.blade.php
@@ -5,7 +5,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>@yield('title', config ('app.name'))</title>
+        <title>Olga Lukianova's project</title>
 
         @yield('head')
 
