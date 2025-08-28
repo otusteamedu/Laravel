5bcd01f7e09b84e0397a90f0ab187c6949327567
mrkamischinmailru <mr.kamischin@mail.ru>
2025-08-28 17:49:23 +0300

    fix: тестирвоание добавления записи

--- app/Http/Controllers/News/Create.php
+++ app/Http/Controllers/News/Create.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\News;
 
 use App\Models\News;
+use App\Models\User;
 use Illuminate\Support\Str;
 use Illuminate\Contracts\Auth\Factory;
 use Illuminate\Contracts\Validation\Factory as ValidationFactory;
@@ -13,7 +14,7 @@ use Illuminate\Support\Facades\Auth;
 use \Illuminate\Contracts\Foundation\Application;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
-use App\Events\NotificationEvent;
+use App\Events\NotificationNewsCreatedEvent;
 class Create
 {
     public function create(): View
@@ -54,10 +55,10 @@ class Create
         $news->text = $request->input('text');
         $news->link = Str::slug($request->input('name'));
         $news->preview = Str::limit($request->input('text'), 20, '...');
-        $news->user_id = Auth::id();
+        $news->user_id = Auth::id() ?? User::latest()->first()->id;
         $news->create_at = Carbon::now()->format('Y-m-d');
         $news->save();
-        event(new NotificationEvent($news));
+        event(new NotificationNewsCreatedEvent($news->id));
         return redirect()
             ->route('news.index')
             ->with('success', 'Пост успешно создан')
