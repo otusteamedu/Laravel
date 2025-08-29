0d9936d1a1c30ba985795df575ee154399b8dfea
mrkamischinmailru <mr.kamischin@mail.ru>
2025-08-29 16:23:53 +0300

    fix: добавление аннотаций  для моделей

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -5,31 +5,33 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Database\Eloquent\Relations\morphMany;
+use Illuminate\Database\Eloquent\Relations\belongsTo;
 class Comment extends Model
 {
     use HasFactory;
     /**
     @return BelongsTo<User,string>
     */
+    #[belongsTo(User::class, "user_id")]
     function author()
     {
-        $class = Relation::getMorphedModel('user');
-        return $this->belongsTo($class, "user_id");
+        return $this->belongsTo(User::class, "user_id");
     }
     /**
     @return BelongsTo<News>
     */
+    #[belongsTo(News::class)]
     function news()
     {
-        $class = Relation::getMorphedModel('news');
-        return $this->belongsTo($class);
+        return $this->belongsTo(News::class);
     }
     /**
     @return morphMany<Like,string>
     */
+    #[morphMany(Like::class, 'liked')]
     public function likes()
     {
-        $class = Relation::getMorphedModel('like');
-        return $this->morphMany($class, 'liked');
+        return $this->morphMany(Like::class, 'liked');
     }
 }
