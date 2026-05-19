a24f465e6091d3d8c6c87cc1695a69b2e270584a
Ilya Nurullin <nurullinin@mail.ru>
2026-05-19 19:44:07 +0300

    код с занятия логирование, коллекции, файлы

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,6 +29,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'is_admin' => 'boolean',
         ];
     }
 
@@ -40,7 +41,7 @@ class User extends Authenticatable
         return Str::of($this->name)
             ->explode(' ')
             ->take(2)
-            ->map(fn ($word) => Str::substr($word, 0, 1))
+            ->map(fn($word) => Str::substr($word, 0, 1))
             ->implode('');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('home');
@@ -10,5 +13,61 @@ Route::middleware(['auth', 'verified'])->group(function () {
 });
 
 Route::apiResource('posts', PostController::class)->middleware('auth');
+
+Route::get('/log', function (Request $request) {
+    $amount = '200.00';
+    $context = ['req' => $request, 'amount' => $amount];
+    Log::info('Log message', $context);
+    Log::debug('Amount', compact('amount'));
+
+    return "ok";
+});
+
+Route::get('/collect', function (Request $request) {
+    $users = User::all();
+
+    dump($users
+        ->filter(fn($user) => $user->is_admin)
+        ->map(fn($user) => $user->name));
+
+    dump($users->filter->is_admin->map->name);
+
+    return "ok";
+});
+
+Route::get('/lc', function () {
+    $users = User::cursor();
+
+    $admins = $users->filter->is_admin;
+
+    foreach ($admins as $admin) {
+        echo '' . $admin->name . '<br>';
+    }
+
+    return 'ok';
+});
+
+Route::get('/create-html', function () {
+    Storage::disk('public')->put('test.html', '<h1>file</h1>');
+
+    return 'ok';
+});
+
+Route::get('/private-link', function () {
+    Storage::put('private.html', '<h1>private</h1>');
+
+    $url = Storage::temporaryUrl('private.html', now()->addMinutes(5));
+
+    return $url;
+});
+
+Route::post('/upload-file', function () {
+    $file = request()->file('image');
+    $name = $file->getClientOriginalPath();
+    $fileName = $file->storeAs('images', $name, 'public');
+
+    return Storage::disk('local')->temporaryUrl($fileName, now()->addMinutes(5));
+
+})->name('upload-file');
 
 require __DIR__ . '/settings.php';
